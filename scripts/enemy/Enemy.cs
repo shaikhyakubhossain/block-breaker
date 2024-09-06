9dfd7c7fc4c4e2cdf30817c75b6ff3cc3cf5fd3b
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-06 23:48:44 +0530

    Update Enemy script, modify deploy.yml

--- scripts/enemy/Enemy.cs
+++ scripts/enemy/Enemy.cs
@@ -1,14 +1,24 @@
 using Godot;
-using System;
 
 public partial class Enemy : CharacterBody2D
 {
-	[Export] public float Speed = 300.0f;
+	[Export] public float Speed = 100.0f;
 	[Export] public float JumpVelocity = -400.0f;
+	[Export] public Node2D target;
+
+		Vector2 velocity;
+
+
+	public override void _Ready()
+	{
+
+		target = GetParent().GetNode<CharacterBody2D>("player");
+
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+		velocity = Velocity;
 
 		// Add the gravity.
 		if (!IsOnFloor())
@@ -17,9 +27,19 @@ public partial class Enemy : CharacterBody2D
 		}
 
 
+		Vector2 direction = (target.GlobalPosition - GlobalPosition).Normalized();
+		velocity.X = direction.X * Speed;
+
 		
+		jumpTowardsTarget();
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+	private void jumpTowardsTarget(){
+		if(target.GlobalPosition.Y <= GlobalPosition.Y - 60 && IsOnFloor()){
+			velocity.Y = JumpVelocity;
+		}
 	}
 }
